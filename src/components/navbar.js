f81fc51ff886831cc96880e7a35e860a6c53f810
Nabil Mahel <nabilmhl-dev@outlook.com>
2024-05-06 10:19:24 +0200

    implementation des dependences et de sass

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function navbar() {
+  return (
+    <>
+    <nav className='navbar'>
+        <div className='navbar-container'>
+
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default navbar
