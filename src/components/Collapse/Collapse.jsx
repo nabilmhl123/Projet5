53beaadf00118a817f12be6fb21dec185b5aa785
Nabil Mahel <nabilmhl-dev@outlook.com>
2024-07-17 17:06:05 +0200

    correctioncollapse

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,23 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 
 const Collapse = ({ title, content }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef(null);
 
   const toggleOpen = () => {
     setIsOpen(!isOpen);
   };
 
+  useEffect(() => {
+    if (isOpen) {
+      contentRef.current.style.height = contentRef.current.scrollHeight + 'px';
+    } else {
+      contentRef.current.style.height = '0px';
+    }
+  }, [isOpen]);
+
   return (
     <div className="collapse">
       <button className="collapse-button" onClick={toggleOpen}>
         {title}
-        <FontAwesomeIcon icon={isOpen ? faChevronDown : faChevronUp} />
+        <FontAwesomeIcon
+          icon={faChevronUp}
+          className={`collapse-icon ${isOpen ? 'open' : ''}`}
+        />
       </button>
-      <div className={`collapse-content ${isOpen ? 'open' : ''}`}>
-        {content}
+      <div
+        ref={contentRef}
+        className={`collapse-content ${isOpen ? 'open' : ''}`}
+        style={{ height: '0px', overflow: 'hidden', transition: 'height 0.3s ease' }}
+      >
+        <div className="collapse-content-inner">
+          {content}
+        </div>
       </div>
     </div>
   );
